bf7659f08e82753a74bfe9af96be3c0605b72d4f
MarauderPixie <tobias.anton.85@gmail.com>
2022-08-03 00:55:57 +0200

    fuck this

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <Experiment title="magpie demo">
-    <InstructionScreen :title="'Welcome'">
-      This is a sample introduction screen.
-    </InstructionScreen>
+  <Experiment title="Institut für Kognitionswissenschaften - Universität Osnabrück" :wide=true>
+    
+    <Screen v-bind="$attrs" title="Registrierung für Probandenstunden">
+    <Slide>
+      <p>Um eine Probandenstunde zu erhalten, benötigen wir deinen Namen, deine Email-Adresse und deine Matrikelnummer.</p>
+      <p>Diese Infos werden unabhängig von den Experimentaldaten gespeichert und sind nicht auf diese zurückzuführen.</p>
+      <hr />
+          <p>
+            <label>
+              Studiengang: 
+              <DropdownInput
+                :options="['', 'Cognitive Science (Uni Osnabrück)', 'Psychologie (Uni Bremen)', 'andere']"
+                :response.sync="$magpie.measurements.uni" 
+              />
+            </label>
+          </p>
+          
+          <p>
+            <label>
+              Matrikelnummer:
+              <input
+                v-model="$magpie.measurements.matrikel"
+                type="text"
+                placeholder="3.14159265"
+              />
+            </label>
+          </p>
+          
+          <p>
+            <label>
+              Vor- und Nachname:
+              <input
+                v-model="$magpie.measurements.name"
+                type="text"
+                placeholder="Quentin Quaderkopf"
+              />
+            </label>
+          </p>
+          
+          <p>
+            <label>
+              Email-Adresse:
+              <input
+                v-model="$magpie.measurements.email"
+                type="text"
+                placeholder="uni@osnabrem.de"
+              />
+            </label>
+          </p>
+
+        <p>
+          <button @click="$magpie.saveAndNextScreen()">Absenden</button>
+        </p>
+      </Slide>
+    </Screen>
+
+    <!-- <Screen v-if="!$magpie.debug">
+      <Slide>
+        <p>Einen Augenblick, Daten werden übermittelt.</p>
+        <Wait :time="0" @done="submit(() => $magpie.nextSlide())" />
+      </Slide>
+      <Slide>
+        <div style="width: 60%; text-align: center;">
+          <p>Deine Daten wurden übermittelt und du kannst dir voraussichtlich Ende September deine VPh eintragen lassen. Solltest du sie vorher benötigen, melde dich gerne einfach bei <a href="tanton@uos.de">tanton@uos.de</a>.</p>
+          <p>Noch einmal vielen Dank für deine Teilnahme!</p>
+        </div>
+      </Slide>
+    </Screen>
+    <DebugResultsScreen v-else /> -->
 
     <SubmitResultsScreen />
+    
   </Experiment>
 </template>
 
 <script>
-import _ from 'lodash';
-
 export default {
-  name: 'App',
-  data() {
-    return {};
-  },
-  computed: {
-    // Expose lodash to template code
-    _() {
-      return _;
-    }
-  }
+  name: 'VP-Hours'
 };
 </script>
+<style>
+.header {
+  background-image: url("../public/UOS-Logo_RotFond_sRGB_v01.jpg"), url("../public/logo_ub_2021.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: left top, right top;
+  text-align: left;
+  height: 64px;
+  width: auto;
+  margin-bottom: 20px;
+}
+</style>
--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,11 +1,11 @@
 export default {
-  experimentId: '2',
-  serverUrl: 'https://magpie.jemu.name/api/submit_experiment/',
+  experimentId: '4',
+  serverUrl: 'magpie.jemu.name/api/submit_experiment/',
   socketUrl: 'wss://magpie.jemu.name/socket',
   // this will be used in prolific mode
   completionUrl: 'https://...',
   contactEmail: 'tanton@uos.de',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug',
+  mode: 'directLink',
   language: 'de'
 };
