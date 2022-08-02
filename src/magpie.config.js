19456cb647454b9d79405837046d963f88a7b873
MarauderPixie <tobias.anton.85@gmail.com>
2022-08-02 12:42:38 +0200

    lil creds

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -4,8 +4,8 @@ export default {
   socketUrl: 'wss://magpie-refactored-2.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://...',
-  contactEmail: 'test@random.com',
+  contactEmail: 'tanton@uos.de',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'debug',
-  language: 'en'
+  language: 'de'
 };
