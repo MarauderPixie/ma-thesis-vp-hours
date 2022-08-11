9a40a9a10050467540bee8515dbb7ebb65745ab2
MarauderPixie <tobias.anton.85@gmail.com>
2022-08-11 16:44:53 +0200

    go live!

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,5 +1,5 @@
 export default {
-  experimentId: '5',
+  experimentId: '8',
   serverUrl: 'https://magpie.jemu.name/',
   socketUrl: 'wss://magpie.jemu.name/socket',
   // this will be used in prolific mode
